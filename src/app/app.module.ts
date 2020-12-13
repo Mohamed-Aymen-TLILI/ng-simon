168b1f1bc557c6f94ae53a41c387cec8f7664c23
libra1919 <aymentli@gmail.com>
2020-12-13 19:21:36 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './components/game/game.component';
+import { GameButtonComponent } from './components/game/game-button/game-button.component';
+
+import { GameStateService } from './services/game-state.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, GameComponent, GameButtonComponent],
+  imports: [BrowserModule],
+  providers: [GameStateService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
